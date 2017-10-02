1ad891bca6e17009a94751f83845ff76346d8d44
Kyle Carter <kylec32@gmail.com>
2017-10-01 19:06:19 -0600

    Quicksort in Javascript

---
+++ quicksort.js
@@ -0,0 +1,33 @@
+
+function quickSort(array) {
+    if(array.length <= 1) {
+        return array;
+    } else if(array.length == 2) {
+        if(array[0] <= array[1]){
+            return array;
+        } else {
+            return [array[1],array[0]];
+        }
+    }
+
+    var prePivot = [];
+    var postPivot = [];
+    var pivotPoint = Math.floor(array.length/2);
+    var pivot = array[pivotPoint];
+    for(var i=0; i < array.length; i++){
+        if(i == pivotPoint) {
+            continue;
+        }
+        else if(array[i]<=pivot) {
+            prePivot.push(array[i]);
+        } else {
+            postPivot.push(array[i]);
+        }
+    }    
+
+    return [...quickSort(prePivot),pivot,...quickSort(postPivot)];
+} 
+
+var b= [7,1,3,1,9,6];
+console.log("Elements Before Bubble sort-->",b);
+console.log("Elements After Bubble Sort-->",quickSort(b));
